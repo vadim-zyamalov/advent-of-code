7de86b67f8e4a02137adb9058b38023452f4c357
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-12-03 19:03:53 +0300

    8-th day of 2015

---
+++ 2015/day-08/part1.py
@@ -0,0 +1,31 @@
+chars = 0
+codes = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        string = []
+        for letter in line.strip():
+            string.append(letter)
+        i = 0
+        while i < len(string):
+            if string[i] == '"':
+                codes += 1
+            elif string[i] == '\\':
+                codes += 1
+                if string[i+1] == '"':
+                    codes += 1
+                    chars += 1
+                    i += 1
+                elif string[i+1] == "\\":
+                    codes += 1
+                    chars += 1
+                    i += 1
+                elif string[i+1] == 'x':
+                    codes += 3
+                    chars += 1
+                    i += 3
+            else:
+                codes += 1
+                chars += 1
+            i += 1
+print(codes - chars)
---
+++ 2015/day-08/part2.py
@@ -0,0 +1,35 @@
+chars_old = 0
+chars_new = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        string = []
+
+        for letter in line.strip():
+            string.append(letter)
+        chars_new += 2
+        i = 0
+        while i < len(string):
+            if string[i] == '"':
+                chars_old += 1
+                chars_new += 2
+            elif string[i] == '\\':
+                chars_old += 1
+                chars_new += 2
+                if string[i+1] == '"':
+                    chars_old += 1
+                    chars_new += 2
+                    i += 1
+                elif string[i+1] == "\\":
+                    chars_old += 1
+                    chars_new += 2
+                    i += 1
+                elif string[i+1] == 'x':
+                    chars_old += 3
+                    chars_new += 3
+                    i += 3
+            else:
+                chars_old += 1
+                chars_new += 1
+            i += 1
+print(chars_new - chars_old)
