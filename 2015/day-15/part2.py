4959b2bafe7f7b3f109ab5ed64cecf3889ade4c2
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-12-07 17:51:06 +0300

    Updated 15-th day of 2015
    Ru: Я решил доработать решение и ручками реализовать product. Ну что ж,
    сказано -- сделано.
    
    Разумеется, нативный код, реализованный, несомненно на Си (или C++),
    работает гораздо быстрее моего, написанного на питоне. 22 секунды против
    160-200. Но, главное, это мой код, мое новое знание.

--- 2015/day-15/part2.py
+++ 2015/day-15/part2.py
@@ -1,10 +1,25 @@
-from itertools import product
+# from itertools import product
+import time
 
 ingredients = []
 
 
+def self_product(data, repeat=2):
+    n = len(data)
+    counter = [0 for _ in range(repeat)]
+    while not all(i == (n - 1) for i in counter):
+        pos = repeat - 1
+        while True:
+            counter[pos] = (counter[pos] + 1) % n
+            if counter[pos] == 0:
+                pos -= 1
+            else:
+                break
+        yield [data[i] for i in counter]
+
+
 def spoons_correct(n, msp=100):
-    for i in product([i for i in range(0, msp + 1)], repeat=n):
+    for i in self_product([i for i in range(0, msp + 1)], repeat=n):
         if sum(i) != 100:
             continue
         yield i
@@ -26,6 +41,7 @@ def score(recipe, ingredients):
     return ans
 
 
+t0 = time.time()
 with open("input.txt", "r") as f:
     index = 0
     for line in f:
@@ -38,4 +54,5 @@ for s in spoons_correct(len(ingredients), 100):
     tmp = score(s, ingredients)
     ans = tmp if ans < tmp else ans
 
-print(ans)
+print('{}, elapsed: {}'.format(ans,
+                               time.time() - t0))
