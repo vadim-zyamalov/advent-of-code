98004ddc57ed141c7c163ffafe73b06053616dab
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-12-10 09:21:05 +0300

    10-th day of 2021
    Ru: очень простая задача с довольно очевидным решением.

---
+++ 2021/day-10/part1.py
@@ -0,0 +1,25 @@
+with open("./input.txt", "r") as f:
+    stack = []
+    answer = 0
+    for line in f:
+        for char in line.strip():
+            if char in '([{<':
+                stack.append(char)
+            else:
+                tmp = stack.pop()
+                if (char == ')') and (tmp != '('):
+                    answer += 3
+                    break
+                if (char == ']') and (tmp != '['):
+                    answer += 57
+                    break
+                if (char == '}') and (tmp != '{'):
+                    answer += 1197
+                    break
+                if (char == '>') and (tmp != '<'):
+                    answer += 25137
+                    break
+
+print("Part 1: {}".format(answer))
+
+
---
+++ 2021/day-10/part2.py
@@ -0,0 +1,38 @@
+with open("./input.txt", "r") as f:
+    answer = []
+    for line in f:
+        stack = []
+        lineOk = True
+        for char in line.strip():
+            if char in '([{<':
+                stack.append(char)
+            else:
+                tmp = stack.pop()
+                if (char == ')') and (tmp != '('):
+                    lineOk = False
+                if (char == ']') and (tmp != '['):
+                    lineOk = False
+                if (char == '}') and (tmp != '{'):
+                    lineOk = False
+                if (char == '>') and (tmp != '<'):
+                    lineOk = False
+        if not lineOk:
+            continue
+        result = 0
+        while len(stack) > 0:
+            tmp = stack.pop()
+            result *= 5
+            if tmp == '(':
+                result += 1
+            if tmp == '[':
+                result += 2
+            if tmp == '{':
+                result += 3
+            if tmp == '<':
+                result += 4
+        answer.append(result)
+
+answer.sort()
+print("Part 2: {}".format(answer[len(answer) // 2]))
+
+
