9fa78ba9cc99fe5b3796ce94e8e9c754bef7c8df
Vadim Zyamalov <vadim.zyamalov@gmail.com>
2021-12-12 14:34:20 +0300

    19-th day of 2015
    Ru: двоякое впечатление. С одной стороны, я нашел правильную идею,
    работающую на малых данных. С другой стороны, на больших данных она
    работает не очень чтобы уж, слишком долго. Прочитанная на днях идея о
    том, что линейные алгоритмы лучше, подтверждается.
    
    Решение, которое работает, использует идею стека из решения
    @emilyskidsister. Основная идея этого решения такая же, что и у меня, но
    она реализована через стек и линейный проход, а не через рекурсию.
    
    Так что я и решил, и не решил вторую звездочку. Как-то так.

--- 2015/day-19/part2.py
+++ 2015/day-19/part2.py
@@ -1,33 +1,11 @@
-replacements = {}
+replacements = []
 molecule = ''
-
-MAX_LEN = 1000
-
-
-def alen(molecule):
-    return sum(1 for _ in atomize(molecule))
-
-
-def atomize(molecule):
-    tmp = ''
-    length = len(molecule)
-    start = 0
-    for i in range(length):
-        if str.isupper(molecule[i]) and tmp:
-            yield tmp, start, i - 1
-            tmp = ''
-        tmp += molecule[i]
-        start = i
-    if tmp:
-        yield tmp, start, length - 1
+filter = set()
 
 
 def process(molecule, replacements, echo):
     length = len(molecule)
-    if length < 1:
-        return MAX_LEN
-    if molecule in replacements and \
-        'e' in replacements[molecule]:
+    if molecule == 'e':
         return 1
     pool = []
     for i in range(length):
@@ -35,25 +13,24 @@ def process(molecule, replacements, echo):
             continue
         if echo:
             print("{}/{}".format(i, length))
-        for k in replacements:
-            if molecule[i:].startswith(k):
-                for r in replacements[k]:
-                    if r != 'e':
-                        tmp = process(molecule[:i] + r + molecule[(i+len(k)):], replacements, False)
-                        if tmp < MAX_LEN:
-                            pool.append(tmp)
-    if pool:
+        for fst, snd in replacements:
+            if molecule[i:].startswith(snd):
+                r = molecule[:i] + fst + molecule[(i+len(snd)):]
+                if r not in filter:
+                    filter.add(r)
+                    tmp = process(r, replacements, False)
+                    if tmp is not None:
+                        pool.append(tmp)
+    if pool != []:
         return 1 + min(pool)
-    return MAX_LEN
+    return None
 
 
 with open("input.txt", "r") as f:
     for line in f:
         if "=>" in line:
             key, _, val = line.strip().partition(" => ")
-            if val not in replacements:
-                replacements[val] = []
-            replacements[val].append(key)
+            replacements.append((key, val))
         elif line.strip() == "":
             continue
         else:
---
+++ 2015/day-19/part2b.py
@@ -0,0 +1,38 @@
+replacements = []
+molecule = ''
+
+filter = set()
+
+def process(molecule, replacements):
+    stack = []
+
+    stack.append((len(molecule), 0, molecule))
+    filter.add(molecule)
+
+    while stack != []:
+        _, steps, state = stack.pop()
+    
+        if state == 'e':
+            return steps
+
+        length = len(state)
+        for i in range(length):
+            for fst, snd in replacements:
+                if state[i:].startswith(snd):
+                    tmp = state[:i] + fst + state[(i + len(snd)):]
+                    if tmp not in filter:
+                        filter.add(tmp)
+                        stack.append((len(tmp), steps + 1, tmp))
+
+
+with open("input.txt", "r") as f:
+    for line in f:
+        if "=>" in line:
+            key, _, val = line.strip().partition(" => ")
+            replacements.append((key, val))
+        elif line.strip() == "":
+            continue
+        else:
+            molecule = line.strip()
+
+print("Part 2: {}".format(process(molecule, replacements)))
