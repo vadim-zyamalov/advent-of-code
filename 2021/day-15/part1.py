167a509c9039725a233f5936c435368584b4f459
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-12-15 19:34:03 +0300

    15-th day of 2021
    Ru: да, да, признаюсь, я -- не программист, CS мне не преподавали
    (точнее то, что преподавали, на полноценное CS не тянет, только на
    введение в тему). Так что вторую часть я подсмотрел.
    
    На самом деле, хорошо, что подсмотрел. Без этого я бы алгоритм Дейкстры
    не понял так как все описания, что я видел до этого, отличались "умом и
    сообразительностью". Другими словами они с места в карьер начинали
    описывать быстрый и оптимизированный метод, включающий оцереди и тому
    подобные изыски.
    
    Тем не менее, применение алгоритма Дейкстры в классическом варианте (с
    удалением вершин) для данной задачи неэффективно из-за числа точек (2,5
    миллионов). На странице в Википедии (https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue) и в посте на Reddit
    (https://www.reddit.com/r/adventofcode/comments/rgqzt5/comment/hon7qbz/?utm_source=share&utm_medium=web2x&context=3)
    приведен вариант для добавления точек, что эффективнее, так как не
    требует постоянного поиска минимального значения в списке расстояний (у
    нас суммы весов) длиной те же 2,5 миллиона.
    
    Хотя вторую звездочку я списал, но зато разобрался с алгоритмом
    Дейкстры.

--- 2021/day-15/part1.py
+++ 2021/day-15/part1.py
@@ -1,50 +1,118 @@
 grid = []
 
 moves = [(-1, 0),
-		 (0, -1)]
+         (0, -1)]
 
 
 def dump(grid):
-	for row in grid:
-		for digit in row:
-			print(digit, end=" ")
-		print()
-	print()
+    for row in grid:
+        for digit in row:
+            print(digit, end=" ")
+        print()
+    print()
+
+
+def neighbours(i, j, grid):
+    dimi, dimj = len(grid), len(grid[0])
+    return [(i + di, j + dj)
+            for (di, dj) in [(-1, 0),
+                             (0, -1),
+                             (1, 0),
+                             (0, 1)]
+            if (0 <= i + di < dimi) and (0 <= j + dj < dimj)]
 
 
 def deep_copy(grid):
-	return [[el for el in raw] for raw in grid]
+    return [[el for el in raw] for raw in grid]
 
+
+def increment(grid):
+    return [[1 if el == 9 else el + 1 for el in row] for row in grid]
+
+
+def append_right(grid, new_grid):
+    return [[el for el in grid[row] + new_grid[row]] for row in range(len(grid))]
+
+
+def append_below(grid, new_grid):
+    return [[el for el in row] for row in grid + new_grid]
 
 def process(grid):
-	dimi, dimj = len(grid), len(grid[0])
-	inner_grid = deep_copy(grid)
-	for step in range(dimi):
-		for s in range(step, dimi):
-			tmpr = []
-			tmpc = []
-			for di, dj in moves:
-				if (0 <= step + di < dimi) and \
-				   (0 <= s + dj < dimj):
-					tmpr.append(inner_grid[step + di][s + dj])
-				if (0 <= s + di < dimi) and \
-				   (0 <= step + dj < dimj):
-					tmpc.append(inner_grid[s + di][step + dj]) 
-			if tmpr:
-				inner_grid[step][s] += min(tmpr)
-			if tmpc and (step != s):
-				inner_grid[s][step] += min(tmpc)
-	return inner_grid
+    dimi, dimj = len(grid), len(grid[0])
+    inner_grid = deep_copy(grid)
+    for step in range(dimi):
+        for s in range(step, dimi):
+            tmpr = []
+            tmpc = []
+            for di, dj in moves:
+                if (0 <= step + di < dimi) and \
+                   (0 <= s + dj < dimj):
+                    tmpr.append(inner_grid[step + di][s + dj])
+                if (0 <= s + di < dimi) and \
+                   (0 <= step + dj < dimj):
+                    tmpc.append(inner_grid[s + di][step + dj])
+            if tmpr:
+                inner_grid[step][s] += min(tmpr)
+            if tmpc and (step != s):
+                inner_grid[s][step] += min(tmpc)
+    return inner_grid
+
+
+def process_dijkstra(grid):
+    dimi, dimj = len(grid), len(grid[0])
+    cost = {}
+    points = []
+    for i in range(dimi):
+        for j in range(dimj):
+            cost[i, j] = 10 * dimi * dimj
+            points.append((i, j))
+    cost[0, 0] = 0
+    
+    while len(points) > 0:
+        min_cost = min([cost[k] for k in cost if k in points])
+        pi, pj = [p for p in points if cost[p] == min_cost][0]
+    
+        points.remove((pi, pj))
+
+        for (ni, nj) in neighbours(pi, pj, grid):
+            if (ni, nj) in points and (cost[ni, nj] > cost[pi, pj] + grid[ni][nj]):
+                cost[ni, nj] = cost[pi, pj] + grid[ni][nj]
+    return cost[dimi - 1, dimj - 1]
+
+
+def process_dijkstra_add(grid):
+    dimi, dimj = len(grid), len(grid[0])
+    cost = {(int(0), int(0)): 0}
+    points = [(0, 0)]
+    
+    for (pi, pj) in points:
+        for (ni, nj) in neighbours(pi, pj, grid):
+            if (ni, nj) in cost and (cost[ni, nj] <= cost[pi, pj] + grid[ni][nj]):
+                continue
+            cost[ni, nj] = cost[pi, pj] + grid[ni][nj]
+            points.append((ni, nj))
+    return cost[dimi - 1, dimj - 1]
 
 
 with open("input.txt", "r") as f:
-	for line in f:
-		if line.strip() == "":
-			continue
-		grid.append([int(digit) for digit in line.strip()])
+    for line in f:
+        if line.strip() == "":
+            continue
+        grid.append([int(digit) for digit in line.strip()])
 
-# dump(grid)
+result = process_dijkstra_add(grid)
+print("Part 1: {}".format(result))
 
-result = process(grid)
-print(result[len(result) - 1][len(result) - 1] - result[0][0])
-# dump(result)
+new_grid = deep_copy(grid)
+inc_grid = deep_copy(grid)
+for i in range(4):
+    inc_grid = increment(inc_grid)
+    new_grid = append_right(new_grid, inc_grid)
+
+inc_grid = deep_copy(new_grid)
+for i in range(4):
+    inc_grid = increment(inc_grid)
+    new_grid = append_below(new_grid, inc_grid)
+
+result = process_dijkstra_add(new_grid)
+print("Part 2: {}".format(result))
