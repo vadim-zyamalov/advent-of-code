a8b48bb82949022c8107c75158d869c6b4d821c7
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-12-10 19:26:04 +0300

    Добавлен 6 день 2022 года.

---
+++ 2022/day-06/part1.py
@@ -0,0 +1,27 @@
+with open("./input.txt", "r", encoding="utf8") as f:
+    tmp = f.read(3)
+    steps = 3
+    d = f.read(1)
+    while d != "\n":
+        steps += 1
+        tmp = tmp + d
+        if len(set(tmp)) == 4:
+            break
+        tmp = tmp[1:]
+        d = f.read(1)
+
+print(f"Part 1: {steps}")
+
+with open("./input.txt", "r", encoding="utf8") as f:
+    tmp = f.read(13)
+    steps = 13
+    d = f.read(1)
+    while d != "\n":
+        steps += 1
+        tmp = tmp + d
+        if len(set(tmp)) == 14:
+            break
+        tmp = tmp[1:]
+        d = f.read(1)
+
+print(f"Part 2: {steps}")
