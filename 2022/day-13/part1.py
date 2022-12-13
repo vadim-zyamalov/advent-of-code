9aa49c4cd3fe48771c022a5af5e930dc0ab22c07
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-12-13 16:47:47 +0300

    Добавлен 13 день 2022 года.
    Господа, это позор и опа :) Не совсем полная, но тем не
    менее. Пришлось подсмотреть решение на reddit.
    
    Мое решение, в общих
    чертах, было написано в том же ключе, что и подсмотренное, но не
    работало :) На самом деле, я допустил ошибку в логике написания
    рекурсивной функции:
    
    1. В первую очередь надо описывать и обрабатывать все (!) пути выхода из
    рекурсии, и лишь потом вводить ее саму.
    2. Рекурсию можно запускать не только для вложенных структурных
    объектов тиа списков внутри списков, но и вообще для любого типа.
    
    Я допустил две вышеприведенные ошибки, в реультате чего упустил один
    из вариантов развития событий --- пустой левый элемент.
    
    Заодно узнал, как создавать собственные функции для сравнения и сортировки.

---
+++ 2022/day-13/part1.py
@@ -0,0 +1,77 @@
+import functools
+
+def parse(chunk):
+    if chunk[0] != '[':
+        return int(chunk)
+    nestlv = 0
+    res = []
+    tmp = ''
+    for c in chunk[1:-1]:
+        match c:
+            case '[':
+                tmp += c
+                nestlv += 1
+            case ']':
+                tmp += c
+                nestlv -= 1
+            case ',' if nestlv == 0:
+                res.append(parse(tmp))
+                tmp = ''
+            case _:
+                tmp += c
+    if tmp != "":
+        res.append(parse(tmp))
+    return res
+
+
+def check(l, r):
+    if (type(l) is int) and (type(r) is int):
+        if l < r:
+            return -1
+        elif l > r:
+            return 1
+        else:
+            return 0
+
+    if type(l) is int:
+        l = [l]
+    if type(r) is int:
+        r = [r]
+
+    if l == [] and r != []:
+        return -1
+    if l != [] and r == []:
+        return 1
+    if l == [] and r == []:
+        return 0
+
+    res = check(l[0], r[0])
+    if res:
+        return res
+    else:
+        return check(l[1:], r[1:])
+
+
+packets = []
+
+with open("./input.txt", "r", encoding="utf8") as f:
+    res = 0
+    num = 0
+    for line1, line2 in zip(f, f):
+        num += 1
+        left = parse(line1.strip())
+        right = parse(line2.strip())
+        packets.append(left)
+        packets.append(right)
+
+        if check(left, right) < 0:
+            res += num
+        f.readline()
+
+print(f"Part 1: {res}")
+
+packets.append([[2]])
+packets.append([[6]])
+packets.sort(key=functools.cmp_to_key(check))
+
+print(f"Part 2: {(1 + packets.index([[2]])) * (1 + packets.index([[6]]))}")
