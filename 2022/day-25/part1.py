9deac583d24f834d9a85938e5c55d6cdff9975a9
Vadim Zyamalov <zyamalov.ve@gmail.com>
2022-12-25 15:51:43 +0300

    Добавлен 25 день 2022 года!
    Merry X-Mas!
    
    Задача --- отдых для утомленного путника. Пришлось подумать над
    алгоритмом, но после того как я нашел методичку, согласно которой
    перевод в симметричные системы счисления осуществляется ПОДБОРОМ
    (подбором, Карл), я решил задачу за 5 минут!
    
    Этот год был интересным, но позорным :)

---
+++ 2022/day-25/part1.py
@@ -0,0 +1,38 @@
+neg = {-2: "=", -1: "-"}
+
+def dec_to_five(number):
+    res = ""
+    while number:
+        number, resid = divmod(number, 5)
+        if resid in [0, 1, 2]:
+            res = str(resid) + res
+        elif resid in [3, 4]:
+            number += 1
+            resid = resid - 5
+            res = neg[resid] + res
+    return res
+
+
+def five_to_dec(number):
+    res = 0
+    power = 1
+    while number:
+        cur = number[-1]
+        number = number[:-1]
+        match cur:
+            case "0" | "1" | "2":
+                res += int(cur) * power
+            case "-":
+                res -= power
+            case "=":
+                res -= 2 * power
+        power *= 5
+    return res
+
+
+with open("./input.txt", "r", encoding="utf8") as f:
+    res = 0
+    for num in f:
+        num = num.strip()
+        res += five_to_dec(num)
+    print(f"Part 1: {dec_to_five(res)}")
