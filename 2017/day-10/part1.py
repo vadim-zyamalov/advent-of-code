d373d5a18cd234a4701f729ba452114f49e1ad91
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2023-10-19 20:49:29 +0300

    Небольшая модификация решения 10 дня 2017 года
    Внезапно оно понадобилось для решения задачи другого дня :)

--- 2017/day-10/part1.py
+++ 2017/day-10/part1.py
@@ -12,33 +12,41 @@ def process_list(length, list_seq, pos, skip):
     return list_seq, pos + length + skip, skip + 1
 
 
-with open("../../_inputs/2017/day-10/input.txt", "r", encoding="utf-8") as f:
-    line = f.read().strip()
-    PROG1 = [int(i) for i in line.split(",")]
-    PROG2 = [ord(c) for c in line]
-    PROG2.extend([17, 31, 73, 47, 23])
+def knot_step(prog):
+    prog = [int(i) for i in prog.split(",")]
 
-LIST = [i for i in range(256)]
-POS = 0
-SKIP = 0
+    LIST = [i for i in range(256)]
+    POS = 0
+    SKIP = 0
 
-for l in PROG1:
-    LIST, POS, SKIP = process_list(l, LIST, POS, SKIP)
-
-print(f"Part 1: {LIST[0] * LIST[1]}")
-
-LIST = [i for i in range(256)]
-POS = 0
-SKIP = 0
-
-for i in range(64):
-    for l in PROG2:
+    for l in prog:
         LIST, POS, SKIP = process_list(l, LIST, POS, SKIP)
 
-res = []
+    return LIST[0] * LIST[1]
 
-for i in range(16):
-    res.append(reduce(lambda x, y: x ^ y, LIST[(16 * i) : (16 * (i + 1))]))
 
-res_str = "".join(f"{i:02x}" for i in res)
-print(f"Part 2: {res_str}")
+def knot_hash(prog, extra=[17, 31, 73, 47, 23]):
+    prog = [ord(c) for c in prog]
+    prog.extend(extra)
+
+    LIST = [i for i in range(256)]
+    POS = 0
+    SKIP = 0
+
+    for i in range(64):
+        for l in prog:
+            LIST, POS, SKIP = process_list(l, LIST, POS, SKIP)
+
+    res = []
+
+    for i in range(16):
+        res.append(reduce(lambda x, y: x ^ y, LIST[(16 * i) : (16 * (i + 1))]))
+
+    return "".join(f"{i:02x}" for i in res)
+
+
+with open("_inputs/2017/day-10/input.txt", "r", encoding="utf-8") as f:
+    line = f.read().strip()
+
+print(f"Part 1: {knot_step(line)}")
+print(f"Part 2: {knot_hash(line)}")
