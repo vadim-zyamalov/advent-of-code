e98477e6cca2d33b49c4e5aed524e00cd833e6a3
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2023-10-23 18:24:55 +0300

    День 15 2017 года
    Немного переписал решение, ознакомившись с решениями на Реддите.
    Убрал кучу однотипных функций.
    Также попробовал использовать оператор `:=`, выглядит интересно.

--- 2017/day-15/part1.py
+++ 2017/day-15/part1.py
@@ -1,20 +1,13 @@
-mod16 = 2 ** 16
-mod31 = 2 ** 31 - 1
+MOD31 = (1 << 31) - 1
+MOD16 = (1 << 16) - 1
 
 
-def gen0(i1, i2, f1=16807, f2=48271, mod=mod31):
-    return (i1 * f1) % mod, (i2 * f2) % mod
-
-
-def gen1(i, f=16807, mod=mod31, crit=4):
-    while True:
-        if i % crit == 0:
-            return i
-        i = (i * f) % mod
+def generate(i, factor, mod=MOD31):
+    return (i * factor) % mod
 
 
 if __name__ == "__main__":
-    with open("../../_inputs/2017/day-15/input.txt", "r", encoding="utf8") as f:
+    with open("_inputs/2017/day-15/input.txt", "r", encoding="utf8") as f:
         inits = []
         for line in f:
             tmp = line.strip().split()
@@ -22,19 +15,22 @@ if __name__ == "__main__":
 
     total = 0
     i1, i2 = inits
+
     for _ in range(40_000_000):
-        if (i1 % mod16) == (i2 % mod16):
+        if (i1 & MOD16) == (i2 & MOD16):
             total += 1
-        i1, i2 = gen0(i1, i2)
+        i1, i2 = generate(i1, 16807), generate(i2, 48271)
 
     print(f"Part 1: {total}")
 
     total = 0
     i1, i2 = inits
     for _ in range(5_000_000):
-        i1, i2 = gen1(i1, f=16807, crit=4), gen1(i2, f=48271, crit=8)
-        if (i1 % mod16) == (i2 % mod16):
+        while (i1 := generate(i1, 16807)) % 4 != 0:
+            pass
+        while (i2 := generate(i2, 48271)) % 8 != 0:
+            pass
+        if (i1 & MOD16) == (i2 & MOD16):
             total += 1
-        i1, i2 = gen0(i1, i2)
 
     print(f"Part 2: {total}")
