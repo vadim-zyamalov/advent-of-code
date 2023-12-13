68fca93b64dd2ed2e7d8abcc3d2ff8d2df1b5609
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2023-12-13 19:23:28 +0300

    Сделал альтернативное решение еще проще. 13/2023

--- 2023/day-13/part2.py
+++ 2023/day-13/part2.py
@@ -2,20 +2,13 @@ def transpose(matrix: list[str]) -> list[str]:
     return list(map(lambda x: "".join(x), list(zip(*matrix))))
 
 
-def check(row: str, idx: int, N: int) -> tuple[bool, int]:
+def check(row: str, idx: int, N: int) -> int:
     if idx < (N // 2):
         lb, ub = 0, idx * 2 + 1
     else:
         lb, ub = (idx + 1) - (N - (idx + 1)), N - 1
-    equal = row[lb : idx + 1] == row[ub:idx:-1]
 
-    if equal:
-        return True, 0
-
-    smudges = sum(
-        1 for el1, el2 in zip(row[lb : idx + 1], row[ub:idx:-1]) if el1 != el2
-    )
-    return False, smudges
+    return sum(1 for el1, el2 in zip(row[lb : idx + 1], row[ub:idx:-1]) if el1 != el2)
 
 
 def reflection(matrix: list[str]) -> tuple[list[int], ...]:
@@ -25,10 +18,10 @@ def reflection(matrix: list[str]) -> tuple[list[int], ...]:
     result_sm = []
 
     for i in range(0, N - 1):
-        equal, smudges = list(zip(*[check(row, i, N) for row in matrix]))
-        if all(equal):
+        smudges = sum(check(row, i, N) for row in matrix)
+        if smudges == 0:
             result_eq.append(i + 1)
-        if sum(smudges) == 1:
+        if smudges == 1:
             result_sm.append(i + 1)
 
     return result_eq, result_sm
@@ -42,30 +35,15 @@ if __name__ == "__main__":
     res1 = 0
     res2 = 0
     with open("_inputs/2023/day-13/input.txt", "r", encoding="utf8") as f:
-        current = []
+        contents = f.read()
 
-        for line in f:
-            line = line.strip()
+        for chunk in contents.split("\n\n"):
+            if chunk == "":
+                break
 
-            if line == "":
-                vr, svr, hr, shr = reflections(current)
+            current = chunk.split("\n")
 
-                for v in vr:
-                    res1 += v
-                for v in hr:
-                    res1 += 100 * v
-
-                for v in svr:
-                    res2 += v
-                for v in shr:
-                    res2 += 100 * v
-
-                current = []
-            else:
-                current.append(list(line))
-
-        if current != []:
-            vr, hr, svr, shr = reflections(current)
+            vr, svr, hr, shr = reflections(current)
 
             for v in vr:
                 res1 += v
