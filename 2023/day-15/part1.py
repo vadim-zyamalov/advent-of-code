f02758c80bda43ccc17574faa6b43962ebe1ced4
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2023-12-15 12:01:57 +0300

    День 15 2023 года

---
+++ 2023/day-15/part1.py
@@ -0,0 +1,56 @@
+def _hash(line: str) -> int:
+    result = 0
+    for ch in line:
+        result += ord(ch)
+        result *= 17
+        result %= 256
+    return result
+
+
+def del_lens(boxes: dict[int, list[tuple[str, int]]], box: int, label: str) -> None:
+    for lens in boxes[box]:
+        if lens[0] == label:
+            boxes[box].remove(lens)
+            break
+
+
+def repl_lens(
+    boxes: dict[int, list[tuple[str, int]]], box: int, label: str, focal: int
+) -> None:
+    found = False
+    for i, lens in enumerate(boxes[box]):
+        if lens[0] == label:
+            boxes[box][i] = (label, focal)
+            found = True
+            break
+    if not found:
+        boxes[box].append((label, focal))
+
+
+def power_lens(boxes: dict[int, list[tuple[str, int]]]) -> int:
+    result = 0
+    for box, lenses in boxes.items():
+        for i, (_, focal) in enumerate(lenses):
+            result += (1 + box) * (1 + i) * focal
+    return result
+
+
+if __name__ == "__main__":
+    res1 = 0
+    boxes = {k: [] for k in range(256)}
+
+    with open("./_inputs/2023/day-15/input.txt", "r", encoding="utf8") as f:
+        for val in f.readline().strip().split(","):
+            res1 += _hash(val)
+
+            if (val[-2] == "=") and val[-1].isdigit():
+                label, focal = val[:-2], int(val[-1])
+                box = _hash(label)
+                repl_lens(boxes, box, label, focal)
+            elif val[-1] == "-":
+                label = val[:-1]
+                box = _hash(label)
+                del_lens(boxes, box, label)
+
+    print(f"Part 1: {res1}")
+    print(f"Part 2: {power_lens(boxes)}")
