bade77614bf15cb6a9f8ff8f75ce30b66fd3520d
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2024-01-18 14:12:33 +0300

    Переделан принцип запуска кода для Intcode

--- year2019/day-02/part1.py
+++ year2019/day-02/part1.py
@@ -1,34 +1,20 @@
-def process(positions):
-    idx = 0
-
-    while positions[idx] != 99:
-        i0, i1, i2 = [positions[idx + i] for i in [1, 2, 3]]
-        match positions[idx]:
-            case 1:
-                positions[i2] = positions[i0] + positions[i1]
-            case 2:
-                positions[i2] = positions[i0] * positions[i1]
-            case _:
-                assert False
-        idx += 4
+from ..intcode.intcode import Intcode
 
 
-if __name__ == "__main__":
-    with open("_inputs/2019/day-02/input.txt", "r", encoding="utf8") as f:
-        for line in f.readlines():
-            numbers = [v for v in map(int, line.strip().split(","))]
+with open("_inputs/2019/day-02/input.txt", "r", encoding="utf8") as f:
+    for line in f.readlines():
+        numbers = [v for v in map(int, line.strip().split(","))]
 
-            _numbers = numbers.copy()
+        computer = Intcode(numbers)
+        computer._regs[1], computer._regs[2] = 12, 2
+        computer.process()
+        print(f"Part 1: {computer.regs[0]}")
 
-            numbers[1], numbers[2] = 12, 2
-            process(numbers)
-            print(f"Part 1: {numbers[0]}")
-
-            for i in range(100):
-                for j in range(100):
-                    numbers = _numbers.copy()
-                    numbers[1], numbers[2] = i, j
-                    process(numbers)
-                    if numbers[0] == 19690720:
-                        print(f"Part 2: {100 * i + j}")
-                        exit()
+        for i in range(100):
+            for j in range(100):
+                computer.reset()
+                computer._regs[1], computer._regs[2] = i, j
+                computer.process()
+                if computer.regs[0] == 19690720:
+                    print(f"Part 2: {100 * i + j}")
+                    exit()
