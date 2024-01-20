1428245f666b5d736f4424168a189cbbad784d65
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2024-01-20 10:20:36 +0300

    Реализовал комбинаторное решение задачи 04/2019

--- 2019/day-04/part1.py
+++ 2019/day-04/part1.py
@@ -1,6 +1,3 @@
-from functools import cache
-
-
 def check(password, func):
     password = str(password)
     if len(password) != 6 or any(
@@ -9,36 +6,6 @@ def check(password, func):
         return False
     n_digits = [password.count(ch) for ch in set(password)]
     return any(func(n) for n in n_digits)
-
-
-@cache
-def fact(n):
-    if n <= 0:
-        return 1
-    return n * fact(n - 1)
-
-
-def b(n, k):
-    if (k == 0) and (n != 0):
-        return 0
-    if (n == 0) and (k != 0):
-        return 1
-    if (n == 0) and (k == 0):
-        return 1
-    return fact(n + k - 1) // (fact(k - 1) * fact(n))
-
-
-def part2(digits=6, gr=2, lower=0, upper=9, fst=False):
-    result = 0
-
-    for i in range(digits - (gr - 1)):
-        for d in range(lower, upper + 1):
-            dl, dr = d - lower, upper - d
-            ld, rd = i, digits - gr - i
-            nl = b(ld, dl)
-            nr = b(rd, dr)
-            result += nl * nr
-    return result
 
 
 if __name__ == "__main__":
---
+++ 2019/day-04/part1a.py
@@ -0,0 +1,50 @@
+from itertools import combinations
+
+
+def num(digits=6, rng=10, cond=lambda x: x >= 2):
+    result = 0
+    for pos in combinations(range(digits + rng - 1), rng - 1):
+        pos = (-1,) + pos + (digits + rng - 1,)
+        if any(
+            cond(q) for q in [i1 - i0 - 1 for i0, i1 in zip(pos[:-1], pos[1:])]
+        ):
+            result += 1
+    return result
+
+
+def count(lower, digits=6, cond=lambda x: x >= 2):
+    result = 0
+    stack = []
+
+    dgts = [9]
+    while lower:
+        lower, d = divmod(lower, 10)
+        dgts = [d] + dgts
+
+    if len(dgts) - 1 < digits:
+        return num(digits=digits, rng=9, cond=cond)
+
+    if len(dgts) - 1 > digits:
+        return 0
+
+    for i, (d0, d1) in enumerate(zip(dgts[:-1], dgts[1:])):
+        if d0 <= d1:
+            stack.append((d0 + 1, digits - i))
+        else:
+            stack.append((d0, digits - i))
+            break
+
+    while stack:
+        d, n = stack.pop()
+        result += num(digits=n, rng=9 - d + 1, cond=cond)
+
+    return result
+
+
+if __name__ == "__main__":
+    with open("_inputs/2019/day-04/input.txt", "r", encoding="utf8") as f:
+        lower, upper = tuple(map(int, f.read().strip().split("-")))
+    print(f"Part 1: {count(lower) - count(upper)}")
+    print(
+        f"Part 1: {count(lower, cond=lambda x: x == 2) - count(upper, cond=lambda x: x == 2)}"
+    )
