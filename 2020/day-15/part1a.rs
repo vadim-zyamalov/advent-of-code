fb12e6fa0ff11dc75fed952f2b70f1dd2a4d7152
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2024-02-15 11:03:25 +0300

    Правки в код 15/2020 на Rust

--- 2020/day-15/part1a.rs
+++ 2020/day-15/part1a.rs
@@ -1,34 +1,46 @@
+use std::fs::read_to_string;
 use std::time::Instant;
+
+const FILEPATH: &str = "../../_inputs/2020/day-15/input.txt";
 
 fn main() {
     let now = Instant::now();
-    let input: [u64; 6] = "18,11,9,0,5,1".split(",")
-                                         .map(|n| n.parse::<u64>().unwrap())
-                                         .collect::<Vec<u64>>()
-                                         .try_into()
-                                         .unwrap();
+
     let maxl = 30_000_000;
+
+    let input: Vec<_> = read_to_string(FILEPATH)
+        .unwrap()
+        .lines()
+        .map(|x| x.trim())
+        .map(String::from)
+        .collect();
+
+    let input: Vec<u64> = input[0]
+        .split(",")
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+
     let mut count = Vec::<u64>::new();
     count.resize(maxl, 0 as u64);
 
-    for (i, num) in input.iter().enumerate() {
-        count[*num as usize] = (i + 1) as u64;
+    for (i, &num) in input.iter().enumerate() {
+        count[num as usize] = (i + 1) as u64;
     }
 
     let mut prev: u64 = input[input.len() - 1];
 
-    for i in 7..=30_000_000 {
+    for i in 7..=maxl {
         if i == 2021 {
             println!("Part 1: {prev}");
         }
 
-        let nxt = if count[prev as usize] != 0 {
-            (i - 1) - count[prev as usize]
+        let nxt: u64 = if count[prev as usize] != 0 {
+            ((i - 1) as u64) - count[prev as usize]
         } else {
             0
         };
 
-        count[prev as usize] = i - 1;
+        count[prev as usize] = (i - 1) as u64;
         prev = nxt;
     }
     println!("Part 2: {prev}");
